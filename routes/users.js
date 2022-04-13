6134ad7f9b8ed2c9fa7a63af2a48118511b43f30
Vivi Ya <viviviyanez@gmail.com>
2022-04-13 17:37:44 +0200

    movies routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const moviesController = require('../controllers/moviesController')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+
+
+router.get('/', moviesController.getAllTheMovies);
+router.get('/:genre', moviesController.getMoviesByGenre);
+router.get('/:title', moviesController.getMoviesByTitle);
+
+
+
+router.get('/auth', moviesController.displayAuthPage);
+
+router.post('/auth/register', moviesController.createNewUser);
+router.post('/auth/loggin', moviesController.logginUser);
+
+
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
 
 module.exports = router;
