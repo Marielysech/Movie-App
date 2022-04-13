43c8020c5a9caa553e9e7ee10e2a85f67fdd8e60
Vivi Ya <viviviyanez@gmail.com>
2022-04-13 17:44:16 +0200

    fixed file name :P

---
+++ routes/movies.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const moviesController = require('../controllers/moviesController')
+
+
+
+
+router.get('/', moviesController.getAllTheMovies);
+router.get('/:genre', moviesController.getMoviesByGenre);
+router.get('/:title', moviesController.getMoviesByTitle);
+
+
+
+router.get('/auth', moviesController.displayAuthPage);
+
+router.post('/auth/register', moviesController.createNewUser);
+router.get('/auth/loggin', moviesController.logginUser);
+
+
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+module.exports = router;
